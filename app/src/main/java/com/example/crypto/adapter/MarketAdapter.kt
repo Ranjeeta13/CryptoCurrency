cd822595ceb0af2c615083edf70453b0749bab1d
Ranjeeta13 <ranjeetapathak13@gmail.com>
2026-02-10 02:30:11 +0530

    Clean repo: remove generated files and keep source only

--- app/src/main/java/com/example/crypto/adapter/MarketAdapter.kt
+++ app/src/main/java/com/example/crypto/adapter/MarketAdapter.kt
@@ -1,10 +1,13 @@
 package com.example.crypto.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.navigation.Navigation.findNavController
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.crypto.R
@@ -13,11 +16,12 @@ import com.example.crypto.fragment.HomeFragmentDirections
 import com.example.crypto.fragment.MarketFragmentDirections
 import com.example.crypto.fragment.WatchListFragmentDirections
 import com.example.crypto.models.CryptoCurrency
+import java.util.Locale
 
 class MarketAdapter(var context: Context, var list: List<CryptoCurrency>,  var type: String) : RecyclerView.Adapter<MarketAdapter.MarketViewHolder>(){
 
 
-    inner class MarketViewHolder(view: View) : RecyclerView.ViewHolder(view){
+     class MarketViewHolder(view: View) : RecyclerView.ViewHolder(view){
          var binding=CurrencyItemLayoutBinding.bind(view)
 
     }
@@ -27,60 +31,84 @@ class MarketAdapter(var context: Context, var list: List<CryptoCurrency>,  var t
     }
 
 
+    @SuppressLint("SetTextI18n")
     override fun onBindViewHolder(holder: MarketViewHolder, position: Int) {
-       val item=list[position]
-        holder.binding.currencyNameTextView.text=item.name
-        holder.binding.currencySymbolTextView.text=item.symbol
 
-        Glide.with(context).load(
-            "https://s2.coinmarketcap.com/static/img/coins/64x64/"+ item.id +".png"
-        ).thumbnail(Glide.with(context).load(R.drawable.spinner))
+        val item = list[position]
+
+        holder.binding.currencyNameTextView.text = item.name
+        holder.binding.currencySymbolTextView.text = item.symbol
+
+        Glide.with(context)
+            .load("https://s2.coinmarketcap.com/static/img/coins/64x64/${item.id}.png")
+            .thumbnail(Glide.with(context).load(R.drawable.spinner))
             .into(holder.binding.currencyImageView)
 
-        Glide.with(context).load(
-            "https://s3.coinmarketcap.com/generated/sparklines/web/7d/usd/"+ item.id +".png"
-        ).thumbnail(Glide.with(context).load(R.drawable.spinner))
+        Glide.with(context)
+            .load("https://s3.coinmarketcap.com/generated/sparklines/web/7d/usd/${item.id}.png")
+            .thumbnail(Glide.with(context).load(R.drawable.spinner))
             .into(holder.binding.currencyChartImageView)
 
-        holder.binding.currencyPriceTextView.text="${String.format("$%.02f",item.quotes[0].price)}"
+        holder.binding.currencyPriceTextView.text =
+            String.format(Locale.US, "$%.02f", item.quotes[0].price)
 
-        if(item.quotes!![0].percentChange24h > 0){
-            holder.binding.currencyChangeTextView.setTextColor(context.resources.getColor(R.color.green))
-            holder.binding.currencyChangeTextView.text= "+ ${String.format("%.02f",item.quotes[0].percentChange24h)}%"
-        }
-        else{
-            holder.binding.currencyChangeTextView.setTextColor(context.resources.getColor(R.color.red))
-            holder.binding.currencyChangeTextView.text= "${String.format("%.02f",item.quotes[0].percentChange24h)}%"
-        }
+        val change = item.quotes[0].percentChange24h
+
+        holder.binding.currencyChangeTextView.setTextColor(
+            ContextCompat.getColor(
+                context,
+                if (change > 0) R.color.green else R.color.red
+            )
+        )
+
+        holder.binding.currencyChangeTextView.text =
+            String.format(
+                Locale.US,
+                if (change > 0) "+ %.02f%%" else "%.02f%%",
+                change
+            )
 
         holder.itemView.setOnClickListener {
-
-            if (type == "home") {
-                findNavController(it).navigate(
-                    HomeFragmentDirections.actionHomeFragmentToDetailsFragment(item)
-                )
-            }
-            else if(type=="market"){
-                findNavController(it).navigate(
-                    MarketFragmentDirections.actionMarketFragmentToDetailsFragment(item)
-                )
-            }
-            else{
-                findNavController(it).navigate(
-                    WatchListFragmentDirections.actionWatchListFragmentToDetailsFragment(item)
-                )
+            when (type) {
+                "home" -> findNavController(it)
+                    .navigate(HomeFragmentDirections.actionHomeFragmentToDetailsFragment(item))
+                "market" -> findNavController(it)
+                    .navigate(MarketFragmentDirections.actionMarketFragmentToDetailsFragment(item))
+                else -> findNavController(it)
+                    .navigate(WatchListFragmentDirections.actionWatchListFragmentToDetailsFragment(item))
             }
         }
-
     }
+
 
     override fun getItemCount(): Int {
        return list.size
     }
 
-    fun updateData(dataItem: List<CryptoCurrency>) {
-        list=dataItem
-        notifyDataSetChanged()
+    fun updateData(newData: List<CryptoCurrency>) {
+        val diffCallback = MarketDiffCallback(list, newData)
+        val diffResult = DiffUtil.calculateDiff(diffCallback)
 
+        list = newData
+        diffResult.dispatchUpdatesTo(this)
     }
+
+    class MarketDiffCallback(
+        private val oldList: List<CryptoCurrency>,
+        private val newList: List<CryptoCurrency>
+    ) : DiffUtil.Callback() {
+
+        override fun getOldListSize() = oldList.size
+        override fun getNewListSize() = newList.size
+
+        override fun areItemsTheSame(oldPos: Int, newPos: Int): Boolean {
+            return oldList[oldPos].id == newList[newPos].id
+        }
+
+        override fun areContentsTheSame(oldPos: Int, newPos: Int): Boolean {
+            return oldList[oldPos] == newList[newPos]
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/crypto/adapter/TopMarketAdapter.kt
+++ app/src/main/java/com/example/crypto/adapter/TopMarketAdapter.kt
@@ -4,17 +4,19 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.Navigation
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.crypto.R
 import com.example.crypto.databinding.TopCurrencyLayoutBinding
 import com.example.crypto.fragment.WatchListFragmentDirections
 import com.example.crypto.models.CryptoCurrency
+import androidx.navigation.findNavController
+import java.util.Locale
 
 class TopMarketAdapter(var context : Context, val list:List<CryptoCurrency>) :RecyclerView.Adapter<TopMarketAdapter.TopMarketViewHolder>(){
 
-    inner class TopMarketViewHolder(view:View) : RecyclerView.ViewHolder(view){
+    class TopMarketViewHolder(view:View) : RecyclerView.ViewHolder(view){
         var binding=TopCurrencyLayoutBinding.bind(view)
     }
 
@@ -23,31 +25,38 @@ class TopMarketAdapter(var context : Context, val list:List<CryptoCurrency>) :Re
     }
 
     override fun onBindViewHolder(holder: TopMarketViewHolder, position: Int) {
-        val item=list[position]
 
-        holder.binding.topCurrencyNameTextView.text=item.name
+        val item = list[position]
 
-        Glide.with(context).load(
-            "https://s2.coinmarketcap.com/static/img/coins/64x64/"+ item.id +".png"
-        ).thumbnail(Glide.with(context).load(R.drawable.spinner))
+        holder.binding.topCurrencyNameTextView.text = item.name
+
+        Glide.with(context)
+            .load("https://s2.coinmarketcap.com/static/img/coins/64x64/${item.id}.png")
+            .thumbnail(Glide.with(context).load(R.drawable.spinner))
             .into(holder.binding.topCurrencyImageView)
 
-        if(item.quotes!![0].percentChange24h > 0){
-            holder.binding.topCurrencyChangeTextView.setTextColor(context.resources.getColor(R.color.green))
-            holder.binding.topCurrencyChangeTextView.text= "+ ${String.format("%.02f",item.quotes[0].percentChange24h)}%"
-        }
-        else{
-            holder.binding.topCurrencyChangeTextView.setTextColor(context.resources.getColor(R.color.red))
-            holder.binding.topCurrencyChangeTextView.text= " ${String.format("%.02f",item.quotes[0].percentChange24h)}%"
-        }
-        holder.itemView.setOnClickListener{
-            Navigation.findNavController(it).navigate(
+        val change = item.quotes[0].percentChange24h
+
+        holder.binding.topCurrencyChangeTextView.setTextColor(
+            ContextCompat.getColor(
+                context,
+                if (change > 0) R.color.green else R.color.red
+            )
+        )
+
+        holder.binding.topCurrencyChangeTextView.text =
+            String.format(
+                Locale.US,
+                if (change > 0) "+ %.02f%%" else "%.02f%%",
+                change
+            )
+
+        holder.itemView.setOnClickListener {
+            it.findNavController().navigate(
                 WatchListFragmentDirections.actionWatchListFragmentToDetailsFragment(item)
             )
         }
-
     }
-
     override fun getItemCount(): Int {
        return list.size
     }
