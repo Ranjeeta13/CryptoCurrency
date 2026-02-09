7b8412d00e224c5bf1decb6084eedcac1df71228
Ranjeeta13 <ranjeetapathak13@gmail.com>
2026-02-10 02:13:39 +0530

    Remove IDE and generated files from repo

--- app/src/main/java/com/example/crypto/MainActivity.kt
+++ app/src/main/java/com/example/crypto/MainActivity.kt
@@ -1,26 +1,41 @@
 package com.example.crypto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.PopupMenu
 import androidx.navigation.fragment.findNavController
 import com.example.crypto.databinding.ActivityMainBinding
+import com.facebook.CallbackManager
 
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navHostFragment =supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
-        val navController= navHostFragment!!.findNavController()
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView)
+        val navController = navHostFragment!!.findNavController()
 
-        val popupMenu =PopupMenu(this,null)
+        val popupMenu = PopupMenu(this, null)
         popupMenu.inflate(R.menu.bottom_nav_menu)
-        binding.bottomBar.setupWithNavController(popupMenu.menu,navController)
+        binding.bottomBar.setupWithNavController(popupMenu.menu, navController)
 
-
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.loginFragment) {
+                binding.bottomBar.visibility = View.GONE
+            } else {
+                binding.bottomBar.visibility = View.VISIBLE
+            }
+        }
     }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        CallbackManager.Factory.create()
+            .onActivityResult(requestCode, resultCode, data)
+    }
+
 }
